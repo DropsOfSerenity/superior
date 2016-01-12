c411879a4e175579caf20ffebcf88f4f67d1511a
Jeremy Venegas <jeremymvenegas@gmail.com>
2016-01-12 13:05:13 -0800

    nav and mobile junk

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,2 +1,11 @@
 //= require jquery
 //= require_tree .
+
+$(document).ready(function() {
+	var body = $('body');
+	var button = $('#toggleNav');
+
+	$(button).click(function() {
+		$(body).toggleClass('open');
+	});
+});
