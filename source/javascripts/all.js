4d34f29e5e011a7d6359a2c4add161e487f1b080
Jeremy Venegas <jeremymvenegas@gmail.com>
2016-01-14 15:05:56 -0800

    root page

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -8,4 +8,24 @@ $(document).ready(function() {
 	$(button).click(function() {
 		$(body).toggleClass('open');
 	});
+
+	// tabs
+	var eztLink = $( ".ezt-nav a" );
+
+  $(eztLink).click(function() {
+    var ezTarget = $(this).closest('ul').attr('ezt');
+    var ezTargetSelector = '[ezt="' + ezTarget + '"] ';
+    var ezIndex = $(this).parent().index() + 1; 
+
+    var currentlyActive = $(this).hasClass('ezt-active');
+
+    if (!currentlyActive) {
+      // update nav
+      $(ezTargetSelector + 'li a').removeClass('ezt-active');
+      $(ezTargetSelector + 'li:nth-child(' + ezIndex + ') a').addClass('ezt-active');
+      //update content
+      $(ezTargetSelector + '.ezt-content').removeClass('ezt-active');
+      $(ezTargetSelector + '.ezt-content:nth-child(' + ezIndex + ')').addClass('ezt-active');
+    }            
+  });
 });
